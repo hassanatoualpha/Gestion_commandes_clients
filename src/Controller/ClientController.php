5c68558b1db4382ede8a1e13e0a8483722de8259
koulibalyamadou10 <koulibalyamadou10@gmail.com>
2025-06-20 21:45:32 +0000

    une

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -11,7 +11,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-
 #[Route('/client')]
 class ClientController extends AbstractController
 {
